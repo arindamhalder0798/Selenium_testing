4c3bccf139c9454a130aa8e396f27674b3840c2e
Arindam Halder <36676669+arindamhalder0798@users.noreply.github.com>
2019-12-03 14:20:11 +0530

    automatically open atg.party and posts party
    Logs response time , auto login with correct credentials, after success login redirects to the atg.party/article , post a party and publishes the current url in a text file.

--- atg_automation/atg_automate.py
+++ atg_automation/atg_automate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 20 10:52:36 2019
+Created on Sat Nov 30 10:52:36 2019
 
 @author: arindam halder
 """
@@ -8,15 +8,31 @@ Created on Sat Jul 20 10:52:36 2019
 import time
 import os
 from selenium import webdriver
+from datetime import datetime
 
 cwd = os.getcwd()
 filePath = os.path.join(cwd, "test.jpg")
+print("\n\n\n Testing has results are as follows:\n")
+# browser = webdriver.Firefox()
+browser = webdriver.Chrome(executable_path = './chrome/78/chromedriver.exe')
+#string fbrowserile to save in log
+log_file = ""
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+log_file = log_file + "Date and time = " + str(dt_string) + "\n\n"
+browser_name = browser.capabilities['browserName']
+version = browser.capabilities['browserVersion']
+# print(browser.capabilities)
+log_file = log_file + "Browser version: " + str(browser_name) + " " + str(version) + "\n"
 
-# browser = webdriver.Chrome(executable_path = './chrome/78/chromedriver.exe')
-browser = webdriver.Firefox()
+log_file = log_file + "Tester Name : Arindam Halder" + "\n"
+
+print(log_file)
 browser.get('https://www.atg.party') 
+time.sleep(2)
+browser.maximize_window()
 
-''' Use Navigation Timing  API to calculate the timings that matter the most '''   
+''' Use Navigation Timing  API to calculate the timings '''   
  
 navigationStart = browser.execute_script("return window.performance.timing.navigationStart")
 responseStart = browser.execute_script("return window.performance.timing.responseStart")
@@ -25,9 +41,12 @@ domComplete = browser.execute_script("return window.performance.timing.domComple
 ''' Calculate the performance'''
 backendPerformance_calc = responseStart - navigationStart
 frontendPerformance_calc = domComplete - responseStart
- 
-print("Back End: %s" % backendPerformance_calc)
-print("Front End: %s" % frontendPerformance_calc)
+
+log_file = log_file + "Back End loading time: " + str(backendPerformance_calc)+ "\n"
+log_file = log_file + "Front End loading time: " + str(frontendPerformance_calc)+ "\n"
+
+print("Back End loading time: %s" % backendPerformance_calc)
+print("Front End loading time: %s" % frontendPerformance_calc)
 
 time.sleep(3)
 
@@ -37,12 +56,13 @@ login_buttton = browser.find_element_by_css_selector('a.additional_signin')
 login_buttton.click()
 time.sleep(3)
 inputEmail = browser.find_element_by_id("email")
-inputEmail.send_keys("wiz_saurabh@rediffmail.com")
+inputEmail.send_keys("kbharathkumar02@gmail.com")
 time.sleep(3)
 inputPassword = browser.find_element_by_id("password")
-inputPassword.send_keys("Pass@123")
+inputPassword.send_keys("Sunnyams@02")
 time.sleep(3)
 signinButton = browser.find_element_by_id("reset_button")
+signinButton.click()
 time.sleep(15)
 browser.get('https://www.atg.party/article')
 time.sleep(9)
@@ -58,6 +78,10 @@ time.sleep(9)
 browser.find_element_by_css_selector('button.btn.btn-post').click()
 time.sleep(9)
 url = browser.current_url
-print(url)
+log_file = log_file + "Url after posting the event: " + str(url)+ "\n\n ===========================================================\n\n\n"
+print("Url after posting the event:",url)
 time.sleep(7)
 browser.quit()
+with open("test_log.txt", "a") as myfile:
+    myfile.write(log_file)
+print("Log file has been saved. Exiting ..")
